f4b253c4a12617a2723aea44bae8d008e79e134a
may4025425 <may4025425@gmail.com>
2019-11-05 00:26:45 +0800

    11/4小更改

--- app/src/main/java/com/may/guess/MainActivity.java
+++ app/src/main/java/com/may/guess/MainActivity.java
@@ -1,5 +1,6 @@
 package com.may.guess;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -22,9 +23,12 @@ import java.util.Random;
 //範例（期中考）：Shop
 
 public class MainActivity extends AppCompatActivity {
-    int guessTime = 0;
-    private int random;
-    String TAG = MainActivity.class.getSimpleName();
+
+    private static final String TAG = MainActivity.class.getSimpleName() ;
+    int counter = 0;
+    private int secret;
+    private EditText edNumber;
+    private TextView edCounter;
 
 
     @Override
@@ -34,58 +38,86 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        random = new Random().nextInt(10)+1;
-        Log.d(TAG,"secret:"+random);
+        //secret = new Random().nextInt(10)+1;
+       // Log.d(TAG,"secret:"+secret);
+        edNumber = findViewById(R.id.number);
+        edCounter = findViewById(R.id.counter);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 reset();
             }
-
         });
-        ;
+        reset();
+        Log.d(TAG,"string"+secret);
     }
 
     public void check(View view) {
-        guessTime+=1;
-        EditText answer = findViewById(R.id.ed_Answer);
-        TextView info = findViewById(R.id.tv_info);
-        TextView guess = findViewById(R.id.tv_guessTime);
-        int  num = Integer.valueOf(answer.getText().toString());
+       // guessTime+=1;
+       // EditText answer = findViewById(R.id.ed_Answer);
+       // TextView info = findViewById(R.id.tv_info);//TextView guess = findViewById(R.id.tv_guessTime);
+       // int  num = Integer.valueOf(answer.getText().toString());
+
+        int num = Integer.parseInt(edNumber.getText().toString());
+        counter++;
+        edCounter.setText(counter+"");
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                reset();
+            }
+        };
+
+        String message = "厲害";
+        if (num < secret) {
+            message = "Bigger";
+            listener = null;
+        } else if (num > secret) {
+            message = "Smaller";
+            listener = null;
+        }
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Hahah")
+                .setMessage("厲害")
+                .setPositiveButton("OK", listener)
+                .show();
 
 
-
-        if (num==random){
+       /* if (num == secret){
            // info.setText("答對了");
+
             new AlertDialog.Builder(MainActivity.this)
                     .setMessage("BINGO")
                     .setTitle("棒棒噠")
                     .setPositiveButton("ok",null)
                     .show();
-        }else if (num<random){
+        }else if (num < secret){
             //info.setText("大一點");
             new AlertDialog.Builder(MainActivity.this)
                     .setMessage("bigger")
                     .setTitle("haha")
                     .setPositiveButton("ok",null)
                     .show();
-        }else if (num>random){
+        }else if (num > secret){
             //info.setText("小一點");
             new AlertDialog.Builder(MainActivity.this)
                     .setMessage("smaller")
                     .setTitle("haha")
                     .setPositiveButton("ok",null)
                     .show();
-        }
-        guess.setText("猜了"+Integer.toString(guessTime)+"次");
+        } */
+
+        edCounter.setText("猜了"+Integer.toString(counter)+"次");
 
 
     }
 
     public void reset(){
-        int secret = new Random().nextInt(10)+1;
-        int num = new Random().nextInt(10)+1;
+        secret = new Random().nextInt(10)+1;
+        counter = 0;
+        edCounter.setText(counter+"");
     }
 
     @Override
